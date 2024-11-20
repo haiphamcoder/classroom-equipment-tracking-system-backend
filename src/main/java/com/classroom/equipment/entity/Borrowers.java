7ca3c35f5ee3d6e3c27b319b473d89932d95e36e
Hai Pham Ngoc <ngochai285nd@gmail.com>
2024-11-21 01:15:53 +0700

    feat: remove unused import

--- src/main/java/com/classroom/equipment/entity/Borrowers.java
+++ src/main/java/com/classroom/equipment/entity/Borrowers.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
